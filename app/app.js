07874387284fccea6c8b8471bdf86786cdaca6ae
Seungmin <shin891211@gmail.com>
2021-09-15 00:58:28 -0400

    working on slide image on main page

--- app/app.js
+++ app/app.js
@@ -3,8 +3,33 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navContainer = document.querySelector('nav.nav-container');
 
 hamburgerMenu.addEventListener('click', () => {
-    navContainer.classList.toggle('active');
+  navContainer.classList.toggle('active');
 })
 
-console.log(navContainer);
-console.log(hamburgerMenu);
+
+
+const carouselImages = document.querySelector('.img-container');
+const slideButtons = document.querySelectorAll('.slide-button');
+const numberOfImages = document.querySelectorAll('.img-container img').length;
+
+let imageIndex = 1;
+let translateX = 0;
+
+slideButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    if (event.target.id === 'previous') {
+      if (imageIndex !== 1) {
+        imageIndex--;
+        translateX += 400;
+      }
+    } else {
+      if (imageIndex !== numberOfImages) {
+        imageIndex++;
+        translateX -= 400;
+      }
+    }
+
+    carouselImages.style.transform = `translateX(${translateX}px)`;
+  })
+})
+

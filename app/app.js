015840ad6642f475e47d3e7e4aa32e3e72b985bc
Seungmin <shin891211@gmail.com>
2021-09-16 00:43:36 -0400

    added gallery slide function

--- app/app.js
+++ app/app.js
@@ -8,28 +8,43 @@ hamburgerMenu.addEventListener('click', () => {
 
 
 
-const carouselImages = document.querySelector('.img-container');
-const slideButtons = document.querySelectorAll('.slide-button');
-const numberOfImages = document.querySelectorAll('.img-container img').length;
+const slideApp = {};
 
-let imageIndex = 1;
-let translateX = 0;
 
-slideButtons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    if (event.target.id === 'previous') {
-      if (imageIndex !== 1) {
-        imageIndex--;
-        translateX += 400;
+slideApp.carouselImages = document.querySelector('.img-container');
+slideApp.slideButtons = document.querySelectorAll('.slide-button');
+slideApp.numberOfImages = document.querySelectorAll('.img-container img').length;
+
+slideApp.imageIndex = 0;
+slideApp.translateX = 0;
+
+slideApp.moveSlide = function () {
+  slideApp.slideButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      if (event.target.id === 'previous') {
+        if (slideApp.imageIndex !== 0) {
+          slideApp.imageIndex--;
+          slideApp.translateX += 400;
+
+        }
+      } else {
+        if (slideApp.imageIndex !== slideApp.numberOfImages - 1) {
+          slideApp.imageIndex++;
+          slideApp.translateX -= 400;
+
+        }
       }
-    } else {
-      if (imageIndex !== numberOfImages) {
-        imageIndex++;
-        translateX -= 400;
-      }
-    }
+      slideApp.carouselImages.style.transform = `translateX(${slideApp.translateX}px)`;
+    })
+  });
+}
 
-    carouselImages.style.transform = `translateX(${translateX}px)`;
-  })
-})
+slideApp.init = function () {
+  slideApp.moveSlide();
+}
+
+slideApp.init();
+
+
+
 
